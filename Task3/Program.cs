abd5e79f1d9fa3b11907ac5afb30fd9cf81990d6
Tasa <greatwillow@mail.ru>
2022-07-06 16:35:59 +0300

    third task

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число.");
+        }
+    }
+
+    return result;
+}
+
+void Sqrt3 (int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write($"{Math.Pow(i,3)}, ");
+    }
+}
+
+int number = GetNumber("Введите число");
+
+Sqrt3 (number);
