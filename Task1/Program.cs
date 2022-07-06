80e805706709a100d1960f7b95f954d55d4de2b9
Tasa <greatwillow@mail.ru>
2022-07-06 16:22:01 +0300

    first task

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            if (result > 9999 && result <= 100000)
+            {
+                break;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Вы ввели не пятизначное число");
+            }
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число.");
+        }
+    }
+
+    return result;
+}
+int number = GetNumber("Введите число");
+string palindrom = Convert.ToString(number);
+
+if (palindrom[0]==palindrom[4] && palindrom[1]==palindrom[3])
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
